6cab2b014e3daf5da74ae6f1b10334ca5d13edb5
Tenoch Z <tenochzaragoza108@proton.me>
2025-12-08 00:32:37 -0800

    got it 'working' in fxml. kinda

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module math130.eclipsedungeon_realfinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens math130.eclipsedungeon_realfinal to javafx.fxml;
+    exports math130.eclipsedungeon_realfinal;
+}
